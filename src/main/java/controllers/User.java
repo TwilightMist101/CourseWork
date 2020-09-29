41b7adef78cf19b7a496e9e81ed04918ec1a4e3b
93330 <93330@farnborough.ac.uk>
2020-09-29 12:01:32 +0100

    Adding Login/Logout Code

--- src/main/java/controllers/User.java
+++ src/main/java/controllers/User.java
@@ -1,16 +1,15 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.UUID;
 
 @Path("user/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -18,6 +17,105 @@ import java.sql.ResultSet;
 
 public class User {
     @GET
+    @Path("login")
+
+    public String loginUser(@FormDataParam("Name") String Name, @FormDataParam("Password") String Password) {
+        System.out.println("Invoked loginUser() on path user/login");
+        try {
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Name = ?");
+            ps1.setString(1, Name);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next()) {
+                String correctPassword = loginResults.getString(1);
+                if (Password.equals(correctPassword)) {
+                    String Token = UUID.randomUUID().toString();
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    ps2.setString(1, Token);
+                    ps2.setString(2, Name);
+                    ps2.executeUpdate();
+
+                    JSONObject userDetails = new JSONObject();
+                    userDetails.put("Name", Name);
+                    userDetails.put("Token", Token);
+                    return userDetails.toString();
+                } else {
+                    return "{\"Error\": \"Incorrect password!\"}";
+                }
+
+            } else {
+                return "{\"Error\": \"Unknown user!\"}";
+            }
+
+        } catch (Exception exception){
+            System.out.println("Database error during /user/login: " + exception.getMessage());
+            return "{\"Error\": \"Server side error!\"}";
+        }
+
+    }
+
+    public static boolean validToken(String Token) {
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
+            ps.setString(1, Token);
+            ResultSet logoutResults = ps.executeQuery();
+            return logoutResults.next();
+        } catch (Exception exception) {
+            System.out.println("Database error during /user/logout: " + exception.getMessage());
+            return false;
+        }
+
+    }
+
+    @POST
+    @Path("logout")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String logoutUser(@CookieParam("Token") String Token) {
+        try {
+            System.out.println("user/logout");
+
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
+            ps1.setString(1, Token);
+            ResultSet logoutResults = ps1.executeQuery();
+            if (logoutResults.next()) {
+
+                int id = logoutResults.getInt(1);
+
+                PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = NULL WHERE UserID = ?");
+                ps2.setInt(1, id);
+                ps2.executeUpdate();
+
+                return "{\"status\": \"OK\"}";
+            } else {
+                return "{\"error\": \"Invalid token!\"}";
+            }
+
+        } catch (Exception exception){
+            System.out.println("Database error during /user/logout: " + exception.getMessage());
+            return "{\"error\": \"Server side error!\"}";
+        }
+
+    }
+
+}
+
+    /*@Path("get/{UserID}")
+    public String getFood(@PathParam("UserID") Integer UserID) {
+        System.out.println("Invoked User.getUser() with UserID " + UserID);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT Name FROM Users WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            if (results.next() == true) {
+                response.put("Name", results.getString(1));
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
     @Path("list")
     public String userList() {
         System.out.println("Invoked User.userList()");
@@ -29,9 +127,9 @@ public class User {
                 JSONObject row = new JSONObject();
                 row.put("UserID", results.getInt(1));
                 row.put("Name", results.getString(2));
-                row.put("Password", results.getInt(3));
-                row.put("Email", results.getInt(4));
-                row.put("Admin", results.getInt(5));
+                row.put("Password", results.getString(3));
+                row.put("Email", results.getString(4));
+                row.put("Admin", results.getBoolean(5));
                 response.add(row);
             }
             return response.toString();
@@ -39,5 +137,5 @@ public class User {
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to list items.\"}";
         }
-    }
+    }*/
 }
