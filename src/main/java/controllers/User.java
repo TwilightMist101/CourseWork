864476b48b53c084cebef5b42f52ad3c9df732ca
93330 <93330@farnborough.ac.uk>
2020-10-02 14:59:30 +0100

    Adding Login System with WebPage

--- src/main/java/controllers/User.java
+++ src/main/java/controllers/User.java
@@ -1,12 +1,15 @@
 package controllers;
 
-import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import server.Main;
 
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONObject;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import java.io.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.UUID;
@@ -16,85 +19,108 @@ import java.util.UUID;
 @Produces(MediaType.APPLICATION_JSON)
 
 public class User {
-    @GET
+    @POST
     @Path("login")
-
-    public String loginUser(@FormDataParam("Name") String Name, @FormDataParam("Password") String Password) {
+    public String loginUser(@FormDataParam("Email") String Email, @FormDataParam("Password") String Password) {
         System.out.println("Invoked loginUser() on path user/login");
-        try {
-            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Name = ?");
-            ps1.setString(1, Name);
-            ResultSet loginResults = ps1.executeQuery();
-            if (loginResults.next()) {
-                String correctPassword = loginResults.getString(1);
-                if (Password.equals(correctPassword)) {
-                    String Token = UUID.randomUUID().toString();
-                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
-                    ps2.setString(1, Token);
-                    ps2.setString(2, Name);
-                    ps2.executeUpdate();
 
-                    JSONObject userDetails = new JSONObject();
-                    userDetails.put("Name", Name);
-                    userDetails.put("Token", Token);
-                    return userDetails.toString();
-                } else {
-                    return "{\"Error\": \"Incorrect password!\"}";
-                }
-
-            } else {
-                return "{\"Error\": \"Unknown user!\"}";
-            }
-
-        } catch (Exception exception){
-            System.out.println("Database error during /user/login: " + exception.getMessage());
-            return "{\"Error\": \"Server side error!\"}";
+        int userID = getUserID(Email, Password);
+        System.out.println("UserID is " + userID);
+        if (userID == -1) {
+            return "{\"Error\": \"Username or password is incorrect.  Are you sure you've registered? \"}";
         }
-
+        String uuid = UUID.randomUUID().toString();
+        String result = updateUUIDinDB(userID, uuid);
+        System.out.println("Generated uuid of " + uuid);
+        if (result.equals("OK")) {
+            JSONObject userDetails = new JSONObject();
+            userDetails.put("sessionToken", uuid);
+            return userDetails.toString();
+        } else {
+            return "{\"Error\": \"Something as gone wrong.  Please contact the administrator with the error code UC-UL. \"}";
+        }
     }
 
-    public static boolean validToken(String Token) {
+    private static int getUserID(String email, String password) {
+        System.out.println("Invoked User.getUserID()");
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
-            ps.setString(1, Token);
-            ResultSet logoutResults = ps.executeQuery();
-            return logoutResults.next();
-        } catch (Exception exception) {
-            System.out.println("Database error during /user/logout: " + exception.getMessage());
-            return false;
-        }
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Email = ? AND Password = ?");
+            ps1.setString(1, email);
+            ps1.setString(2, password);
+            ResultSet resultSet = ps1.executeQuery();
 
+            if (resultSet.next()==false){
+                return -1 ;
+            } else {
+                return resultSet.getInt("UserID");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    private static String updateUUIDinDB(int userID, String uuid) {
+        System.out.println("Invoked User.updateUUIDinDB()");
+
+        try {
+            PreparedStatement statement = Main.db.prepareStatement(
+                    "UPDATE Users SET Token = ? WHERE UserID = ?"
+            );
+            statement.setString(1, uuid);
+            statement.setInt(2, userID);
+            statement.executeUpdate();
+            return "OK";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "Error";
+        }
+    }
+
+    public static int validateSessionCookie(Cookie sessionCookie){
+        String uuid = sessionCookie.getValue();
+        System.out.println("Invoked User.validateSessionCookie(), cookie value " + uuid);
+
+        try {
+            PreparedStatement statement = Main.db.prepareStatement(
+                    "SELECT UserID FROM Users WHERE Token = ?"
+            );
+            statement.setString(1, uuid);
+            ResultSet resultSet = statement.executeQuery();
+            System.out.println("userID is " + resultSet.getInt("UserID"));
+            return resultSet.getInt("UserID");  //Retrieve by column name  (should really test we only get one result back!)
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;  //rogue value indicating error
+
+        }
     }
 
     @POST
-    @Path("logout")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String logoutUser(@CookieParam("Token") String Token) {
+    @Path("add")
+    public String userAdd(@FormDataParam("userID") int userID, @FormDataParam("name") String name,
+                          @FormDataParam("email") String email, @FormDataParam("password") String password,
+                          @FormDataParam("admin") boolean admin) {
+        System.out.println("Invoked User.userAdd()");
+
+        //would be better to test if username taken and if username and password already exist and return useful error message to browswer.
+
         try {
-            System.out.println("user/logout");
+            PreparedStatement statement = Main.db.prepareStatement(
+                    "INSERT INTO Users (UserID, Name, Email, Password, Admin) VALUES (?, ?, ?, ?, ?)"
+            );
+            statement.setInt(1, userID);
+            statement.setString(2, name);
+            statement.setString(3, email);
+            statement.setString(4, password);
+            statement.setBoolean(5, admin);
+            statement.executeUpdate();
+            return "{\"OK\": \"New user has been added successfully. \"}";
 
-            PreparedStatement ps1 = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
-            ps1.setString(1, Token);
-            ResultSet logoutResults = ps1.executeQuery();
-            if (logoutResults.next()) {
-
-                int id = logoutResults.getInt(1);
-
-                PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = NULL WHERE UserID = ?");
-                ps2.setInt(1, id);
-                ps2.executeUpdate();
-
-                return "{\"status\": \"OK\"}";
-            } else {
-                return "{\"error\": \"Invalid token!\"}";
-            }
-
-        } catch (Exception exception){
-            System.out.println("Database error during /user/logout: " + exception.getMessage());
-            return "{\"error\": \"Server side error!\"}";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "{\"Error\": \"Something as gone wrong.  Please contact the administrator with the error code UC-UA. \"}";
         }
-
     }
 
     /*@Path("get/{UserID}")
